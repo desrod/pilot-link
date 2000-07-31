958bc23706665e620d2312d5bbac853c1463b66b
cvs2git <admin@example.com>
2000-07-31 00:51:32 +0000

    This commit was manufactured by cvs2git to create tag 'pilot- link_0_9_3_with_debug_output_and_slp_timeout_patch'.

--- os2configure.cmd
+++
@@ -1,5 +0,0 @@
-/* OS/2 configure script, Mike Jarabek, December 8, 1998 */
-/* just copy the pi-sockaddr.h file, everything in there is fine. */
-'copy include\pi-sockaddr.h.in include\pi-sockaddr.h'
-/* need the pi-config file to be there so that things work fine */
-'touch include\pi-config.h'
