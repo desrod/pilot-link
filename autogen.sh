24ce02285a026195ca267887bb5575a6aebd84ea
JP Rosevear <jpr@ximian.com>
2001-01-23 22:03:48 +0000

    2001-01-23  JP Rosevear  <jpr@ximian.com>
    	* Makefile.in: Add a dist target
    
    	* autogen.sh: add for packaging systems

---
+++ autogen.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+PROJECT=pilot-link
+TEST_TYPE=-d
+FILE=libsock
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+}
+
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+autoconf
+cd $ORIGDIR
+
+$srcdir/configure "$@"
+
+echo 
+echo "Now type 'make' to compile $PROJECT."
