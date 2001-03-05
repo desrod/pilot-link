be367f692b470fdaf93f5461e4faaa6db36f3774
David A. Desrosiers <desrod@gnu-designs.com>
2001-03-05 01:16:22 +0000

    Added man/pilot-addresses.1, tools/empty.in, applied John Marshall's fix for broken directory/Makefile creation. Must fix this when we autoconfiscate the whole tree properly. Rolling some more getopt() fixes into all binaries slowly.

--- src/pilot-install-user.c
+++ src/pilot-install-user.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
    char *progname = argv[0];
    char *device = argv[1];
 
-   PalmHeader(progname);
+//   PalmHeader(progname);
 
    if (argc < 2) {
       fprintf(stderr, "   Assigns your Palm device a user name and unique userid\n\n");
