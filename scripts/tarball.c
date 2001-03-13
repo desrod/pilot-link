0633b363ccfc9922e83ee5ce8855e80440cbf170
JP Rosevear <jpr@ximian.com>
2001-03-13 04:06:18 +0000

    2001-03-12  JP Rosevear  <jpr@ximian.com>
    	* scripts/tarball.c (main): fixes to make it work again, what a
    	horrible hack

--- scripts/tarball.c
+++ scripts/tarball.c
@@ -6,17 +6,17 @@ int main(void)
 {
 	char dir[80],cmd[256];
 	sprintf(dir, "pilot-link-%d.%d.%d", PILOT_LINK_VERSION, PILOT_LINK_MAJOR, PILOT_LINK_MINOR);
-	sprintf(cmd, "rm tarball; cd ..; rm %s; ln -s plk %s", dir, dir);
+	sprintf(cmd, "rm -f tarball; cd ..; rm -f %s; rm -f %s.tar.gz; ln -s pilot-link %s", dir, dir, dir);
 	printf("%s\n",cmd);
 	system(cmd);
-	sprintf(cmd, "cd ..; tar cvf %s.tar %s/", dir, dir);
+	sprintf(cmd, "cd ..; tar chvf %s.tar %s/", dir, dir);
 	printf("%s\n",cmd);
 	fflush(stdout);
 	system(cmd);
 	sprintf(cmd, "cd ..; gzip %s.tar", dir);
 	printf("%s\n",cmd);
 	system(cmd);
-	sprintf(cmd, "cd ..; rm %s", dir);
+	sprintf(cmd, "cd ..; rm -f %s", dir);
 	printf("%s\n",cmd);
 	system(cmd);
 }
