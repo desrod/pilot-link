1c2a09cec37a3f02c6e7b7a87d32d913f866e788
David A. Desrosiers <desrod@gnu-designs.com>
2001-12-23 23:20:27 +0000

    Just changed the version to match HEAD, with tag. 0.10.1-prb

--- include/pi-version.h
+++ include/pi-version.h
@@ -2,11 +2,11 @@
 #define _PILOT_VERSION_H_
 
 #define PILOT_LINK_VERSION 0
-#define PILOT_LINK_MAJOR 9
-#define PILOT_LINK_MINOR 5
+#define PILOT_LINK_MAJOR 10
+#define PILOT_LINK_MINOR 1
 
 /* If releasing a version without a patch number, make sure that this is not
    defined, simply leave blank quotes. */
 
-#define PILOT_LINK_PATCH ".1"
+#define PILOT_LINK_PATCH "-prb"
 #endif	/* _PILOT_VERSION_H_ */
