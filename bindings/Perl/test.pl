8bfc70aa0bc8cf31196aa0544c7766d2f10dfea4
David A. Desrosiers <desrod@gnu-designs.com>
2002-07-11 20:18:16 +0000

    Rewrite of perl binding test, do not dump records to STDOUT. Need to test return of $dlp

--- bindings/Perl/test.pl
+++ bindings/Perl/test.pl
@@ -1,110 +1,189 @@
-sub FooBar { print "Foo: $_[0]\n"; $x = $_[0]; $x =~ s/[aeiou]/\U$&/g; return $x };
+#!/usr/bin/env perl 
 
 use strict;
+# use warnings;
 # use diagnostics;
 use PDA::Pilot;
 use Data::Dumper;
 
-my ($port, $db, $socket, $app, $dlp, $ui, $info, $rec);
+my ($port,      # device port
+    $db,        # database to dump
+    $socket,    # socket descriptor
+    $app,       # AppInfo block
+    $dlp,       # sd = pilot_connect(port);
+    $ui,        # dlp_ReadUserInfo
+    $info,      # dlp_ReadOpenDBInfo
+    $rec);      # record count
 
 if ($ARGV[0]) {
-	$port = $ARGV[0];
+        $port = $ARGV[0];
 } else {
-	print "What port should I use [/dev/pilot]: ";
-	$port = <STDIN>;
-	chop $port;
-	$port ||= "/dev/pilot";
+        print "What port should I use [/dev/pilot]: ";
+        $port = <STDIN>;
+        chomp $port;
+        $port ||= "/dev/pilot";
 }
 
-$socket = PDA::Pilot::openPort($port);
-
-# openPort is the equivalent of
+$socket = PDA::Pilot::openPort($port) or die "$!";
+exit if (!$socket);
 #
-#$socket = PDA::Pilot::socket(PI_AF_SLP, PI_SOCK_STREAM, PI_PF_PADP);
+# openPort is the equivalent of:
 #
-#PDA::Pilot::bind($socket,
-#	{family => PI_AF_SLP, device => $port});
+# $socket = PDA::Pilot::socket(PI_AF_PILOT, PI_SOCK_STREAM, PI_PF_DLP);
+# PDA::Pilot::bind($socket, {family => PI_AF_PILOT, device => $port});
+# PDA::Pilot::listen($socket, 1);
 #
-#PDA::Pilot::listen($socket, 1);
 
-print "Now press the HotSync button\n";
+print "Please press the HotSync button\n";
 
-$dlp = PDA::Pilot::accept($socket);
-
-$PDA::Pilot::UnpackPref{mail}->{3} = sub { $_[0] . "x"};
+$dlp = PDA::Pilot::accept($socket) or die "$!";
 
 print Dumper($dlp);
 
-my @pref = $dlp->getPref('mail', 3);
-@pref = "not available" if not defined $pref[0];
-
-print "Mail preferences: @pref\n";
-
-$ui = $dlp->getUserInfo;
-
-my @b = $dlp->getBattery;
-
-$info = $dlp->getDBInfo(0);
-
-print Dumper($info);
-print "Battery voltage is $b[0], (warning marker $b[1], critical marker $b[2])\n";
-
-$dlp->tickle;
-
-$db = $dlp->open("MemoDB");
-
-print "db class is ", ref $db, "\n";
-
-$rec = $db->getRecord(0);
-
-print "Contents: '$rec->{text}'\n";
-
-$app = $db->getAppBlock;
-
-print Dumper($app);
-print "Categories: @{$app->{categoryName}}\n";
-print Dumper($db->getPref(0));
-print Dumper($db->getPref(1));
-
-$db->close();
-
-$db = $dlp->open("DatebookDB");
-
-print "db class is ", ref $db, "\n";
-
-$rec = $db->getRecord(0);
-
-print "Contents: ", Dumper($rec);
-
-$app = $db->getAppBlock;
-
-print Dumper($app);
-print "Categories: @{$app->{categoryName}}\n";
-
-$db->close();
-
-$db = $dlp->open("MailDB");
-
-if ($db) {
-	print "db class is ", ref $db, "\n";
-	$rec = $db->getRecord(0);
-	print "Contents: ", Dumper($rec);
-	$app = $db->getAppBlock;
-	print Dumper($app);
-	print "Categories: @{$app->{categoryName}}\n";
-	$db->close();
-}
-
-$db = $dlp->open("ExpenseDB");
-if ($db) {
-	print "db class is ", ref $db, "\n";
-	$rec = $db->getRecord(0);
-	print "Contents: ", Dumper($rec);
-	$app = $db->getAppBlock;
-	print Dumper($app);
-	print "Categories: @{$app->{categoryName}}\n";
-}
+dump_sysinfo();
+dump_memopad();
+dump_datebook();
+dump_mail();
+dump_expense();
 
 undef $db; 	# Close database
 undef $dlp; 	# Close connection
-print "Your name is $ui->{name}\n";
+
+############################################################
+# 
+# Read UserInfo, SysInfo, DBInfo
+#
+############################################################
+sub dump_sysinfo {
+	$ui = $dlp->getUserInfo;
+	my @b = $dlp->getBattery;
+	$info = $dlp->getDBInfo(0);
+
+	print "-"x40, "\n";
+	print "Dumping SysInfo\n";
+	print "-"x40, "\n";
+	print Dumper($info);
+
+	print "Battery voltage is $b[0], (warning marker $b[1], critical marker $b[2])\n";
+	print "Your name is $ui->{name}\n";
+}
+
+
+############################################################
+#
+# Dump MemoDB.pdb
+#
+############################################################
+sub dump_memopad {
+	$db = $dlp->open("MemoDB") or die "$!";
+
+	if ($db) {
+		print "-"x40, "\n";
+		print "Dumping MemoDB\n";
+		print "-"x40, "\n";
+		print "db class is ", ref $db, "\n";
+
+		# $rec = $db->getRecord(0);
+		# print "Contents: '$rec->{text}'\n";
+
+		my $app = $db->getAppBlock;
+
+                # Dump all records to STDOUT
+		# print Dumper($app);
+
+		print "Categories: @{$app->{categoryName}}\n";
+		print Dumper($db->getPref(0));
+		print Dumper($db->getPref(1));
+		$db->close();
+	}
+}
+
+############################################################
+#
+# Dump DatebookDB.pdb
+#
+############################################################
+sub dump_datebook {
+	$db = $dlp->open("DatebookDB") or die "$!";
+	if ($db) {
+        	print "-"x40, "\n";
+	        print "Dumping DatebookDB\n";
+        	print "-"x40, "\n";
+		print "db class is ", ref $db, "\n";
+
+		# $rec = $db->getRecord(0);
+		# print "Contents: ", Dumper($rec);
+
+		my $app = $db->getAppBlock;
+
+                # Dump all records to STDOUT
+		# print Dumper($app);
+
+		print "Categories: @{$app->{categoryName}}\n";
+
+		$db->close();
+	}
+}
+
+############################################################
+#
+# Dump MailDB.pdb
+#
+############################################################
+sub dump_mail {
+	$PDA::Pilot::UnpackPref{mail}->{3} = sub { $_[0] . "x"};
+	#
+	# $pref[0] == "Confirm deleted message"
+	# $pref[1] == "Default Mail"
+	# $pref[2] == "Signature text"
+	#
+	my @pref = $dlp->getPref('mail', 0);
+	@pref = "not available" if (!defined $pref[0]);
+	print "Mail preferences: @pref\n";
+
+
+	$db = $dlp->open("MailDB") or die "$!";
+	if ($db) {
+	        print "-"x40, "\n";
+        	print "Dumping MailDB\n";     
+	        print "-"x40, "\n";
+        	print "db class is ", ref $db, "\n";
+
+		# $rec = $db->getRecord(0);
+		# print "Contents: ", Dumper($rec);
+
+		$app = $db->getAppBlock;
+
+		# Dump all records to STDOUT
+		# print Dumper($app);
+
+		print "Categories: @{$app->{categoryName}}\n";
+		$db->close();
+	}
+}
+
+############################################################
+#
+# Dump ExpenseDB.pdb
+#
+############################################################
+sub dump_expense {
+	$db = $dlp->open("ExpenseDB") or die "$!";
+	if ($db) {
+                print "-"x40, "\n";
+                print "Dumping ExpenseDB\n";
+                print "-"x40, "\n";
+                print "db class is ", ref $db, "\n";
+
+		# $rec = $db->getRecord(0);
+		# print "Contents: ", Dumper($rec);
+
+		$app = $db->getAppBlock;
+
+                # Dump all records to STDOUT
+		# print Dumper($app);
+
+		print "Categories: @{$app->{categoryName}}\n";
+	}
+}
+
