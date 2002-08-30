e9b064a0657ea6b7d76c86f1587f80c4485d0c80
David A. Desrosiers <desrod@gnu-designs.com>
2002-08-30 21:29:34 +0000

    Added missing unistd.h include for F_OK

--- src/read-notepad.c
+++ src/read-notepad.c
@@ -23,6 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "config.h"
 #include "pi-source.h"
--- src/read-palmpix.c
+++ src/read-palmpix.c
@@ -23,6 +23,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "pi-file.h"
 #include "pi-socket.h"
