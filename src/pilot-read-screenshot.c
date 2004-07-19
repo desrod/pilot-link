970ee9c8bf4d4145bd833ca9c9dbaf3c0bd585d2
Angus Ainslie <angusa@deltatee.com>
2004-07-19 17:46:53 +0000

    Changed some unsigned char buffers to pi_buffer_t Added support for version 1 screenshots and grayscale

--- src/pilot-read-screenshot.c
+++ src/pilot-read-screenshot.c
@@ -49,6 +49,13 @@
 #define OUT_PPM    1
 #define OUT_PNG    2
 
+struct ss_state {
+   int w, 
+     h, 
+     depth;
+   unsigned char *pix_map;
+};
+   
 struct option options[] =
 {
 	 {"help", no_argument, NULL, 'h'},
@@ -168,14 +175,19 @@ int protect_files (char *name, char *extension)
  *
  * Returns:     Nothing
  *
+ ***********************************************************************/
 #ifdef HAVE_PNG
-void write_png (FILE * f, struct Veo *v, long flags)
+void write_png ( char *fname, struct ss_state *state )
 {
-   unsigned char outBuf[2560];
-   int i;
+   unsigned char *gray_buf;
+   int i, j;
    png_structp png_ptr;
    png_infop info_ptr;
-
+   FILE *f;
+   
+   if( state->depth < 8 )
+     gray_buf = malloc( state->w );
+   
    png_ptr = png_create_write_struct
 	 (PNG_LIBPNG_VER_STRING, png_voidp_NULL,
 	  png_error_ptr_NULL, png_error_ptr_NULL);
@@ -197,28 +209,74 @@ void write_png (FILE * f, struct Veo *v, long flags)
 		return;
 	 }
 
+   f = fopen (fname, "wb");
+	  
    png_init_io (png_ptr, f);
 
-   png_set_IHDR (png_ptr, info_ptr, v->width, v->height,
-				 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-				 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+   if( state->depth < 8 )
+     png_set_IHDR (png_ptr, info_ptr, state->w, state->h,
+		   8, PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE,
+		   PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+   else
+     png_set_IHDR (png_ptr, info_ptr, state->w, state->h,
+		   8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+		   PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
 
    png_write_info (png_ptr, info_ptr);
 
-   for (i = 0; i < v->height; i++)
-	 {
-		Gen24bitRow (flags, i, v, outBuf);
-		png_write_row (png_ptr, outBuf);
-		png_write_flush (png_ptr);
-	 }
+   if( state->depth < 8 )
+     {
+	for (i = 0; i < state->h; i++)
+	  {
+	     for( j=0; j<state->w; j++ )
+	       gray_buf[j] = state->pix_map[i*3*state->w+j*3];
+	     
+	     png_write_row (png_ptr, gray_buf );
+	     png_write_flush (png_ptr);
+	  }
+     }
+   else
+     {
+	for (i = 0; i < state->h; i++)
+	  {
+	     png_write_row (png_ptr, &state->pix_map[i*3*state->w] );
+	     png_write_flush (png_ptr);
+	  }
+     }
 
    png_write_end (png_ptr, info_ptr);
    png_destroy_write_struct (&png_ptr, &info_ptr);
 
+   fclose( f );
+
+   if( state->depth < 8 )
+     free( gray_buf );
 }
 #endif
- ***********************************************************************/
 
+void write_ppm ( char *fname, struct ss_state *state)
+{
+   int i;
+   FILE *f;
+   
+   f = fopen (fname, "wb");
+	  
+   fprintf (f, "P6\n# ");
+
+   fprintf (f, "%s\n", fname );
+
+   fprintf (f, "%d %d\n", state->w, state->h );
+
+   fprintf (f, "255\n" );
+
+   for( i = 0; i < 3*state->h*state->w; i += 3 )
+     fwrite( &state->pix_map[i], 3, 1, f);
+   
+   fclose( f );
+}
+
+	  
 /***********************************************************************
  *
  * Function:    WritePictures
@@ -233,20 +291,22 @@ void write_png (FILE * f, struct Veo *v, long flags)
 void WritePictures (int sd, int db, int type )
 {
    char fname[FILENAME_MAX];
-   FILE *f;
    char extension[8];
-   int i, len, idx = 0, w, h, recs, depth, imgNum = 0;
-   pi_buffer_t *inBuf;
-
-   char *pixelBuf;
-
-//   unsigned char inBufB[320*320];
+   int i, len, idx = 0, recs, imgNum = 0;
+//   unsigned char inBuf[61440], *pixelBuf;
+   pi_buffer_t *inBuf, *pixelBuf;
    unsigned long clut[256], magic;
-   int attr, category, val;
+   int attr, category, val, mask, j, k;
+   struct ss_state state;
+
+   if( type == OUT_PPM )
+     sprintf (extension, ".ppm");
+   else if( type == OUT_PNG )
+     sprintf (extension, ".png");
+   else
+     return;
 
    inBuf = pi_buffer_new (61440);
-
-   sprintf (extension, ".ppm");
 
    if (sd)
      while( 1 )
@@ -261,36 +321,78 @@ void WritePictures (int sd, int db, int type )
 	    }
 	  
 	  idx++;
-	  h = ( inBuf[4] << 8 )+ inBuf[5];
-	  w = ( inBuf[6] << 8 ) + inBuf[7];
-	  recs = inBuf[9];
-	  depth = inBuf[8];
-	  magic = ((unsigned long *)inBuf)[0];
+	  state.h = ( inBuf->data[4] << 8 )+ inBuf->data[5];
+	  state.w = ( inBuf->data[6] << 8 ) + inBuf->data[7];
+	  recs = inBuf->data[9];
+	  state.depth = inBuf->data[8];
+	  magic = ((unsigned long *)inBuf->data)[0];
 	  
-	  if(  magic != 0xBECEDEFE )
+	  if(  magic != 0xBECEDEFE && magic != 0xDEDEFEFE )
 	    {
-	       /* no magic something bad happened */
-	       fprintf( stderr, "No Magic !\n" );
+	       /* no magic must version 1 db */
+//	       fprintf( stderr, "No Magic !\n" );
+	       
+	       state.w = 160;
+	       state.h = 160;
+	       recs = 1;
+	       
+	       switch( len )
+		 {
+		  case 3200:
+		    state.depth = 1;
+		    mask = 1;
+		    break;
 
-	       exit( EXIT_FAILURE );
+		  case 6400:
+		    state.depth = 2;
+		    mask = 3;
+		    break;
+		    
+		  case 12800:
+		    state.depth = 4;
+		    mask = 0x0f;
+		    break;
+		    
+		  case 26624:
+		    state.depth = 8;
+		    break;
+
+		  case 51200:
+		    state.depth = 16;
+		    break;
+
+		  default:
+		    /* unknown record */
+		    /* get next */
+		    fprintf( stderr, "Unknown record" );
+		    continue;
+		 }
+	       
 	    }
 	  
-	  pixelBuf = malloc( h * w * depth / 8 + 10 + 1024 );
+	  pixelBuf 
+	    = pi_buffer_new (state.h * state.w * state.depth / 8 + 10 + 1024);
+
+//	  pixelBuf = malloc( state.h * state.w * state.depth / 8 + 10 + 1024 );
+	  state.pix_map = malloc( state.h * state.w * 3 );
 	  
-	  if( !pixelBuf )
+	  if( !pixelBuf || !state.pix_map )
 	    {
 	       fprintf( stderr, "Memory Allocation failed\n" );
 	       return;
 	    }
 	  
-	  memcpy( pixelBuf, &inBuf[10], len - 10 );
+	  if( magic == 0xBECEDEFE || magic == 0xDEDEFEFE )
+	    memcpy( pixelBuf->data, &inBuf->data[10], len - 10 );
+	  else
+	    memcpy( pixelBuf->data, inBuf->data, len );
 		  
 	  for( i=1; i< recs; i++ )
 	    {
 	       len =
-		 dlp_ReadRecordByIndex (sd, db, idx, inBuf, 0, 0, &attr, &category);
+		 dlp_ReadRecordByIndex (sd, db, idx, inBuf, 0, &attr, &category);
 	       
-	       memcpy( &pixelBuf[i*61440-10], inBuf, len );	  
+	       memcpy( &pixelBuf->data[i*61440-10], inBuf->data, len );	  
 	       
 	       idx++;
 	    }
@@ -300,41 +402,55 @@ void WritePictures (int sd, int db, int type )
 	  protect_files (fname, extension);
 	  
 	  printf ("Generating %s...\n", fname);
-  	  fprintf( stderr, "h: %d w: %d recs: %d bit depth: %d\n", h, w, recs, depth );
+  	  fprintf( stderr, "height: %d width: %d records: %d bit depth: %d\n"
+		   , state.h, state.w, recs, state.depth );
 
-	  f = fopen (fname, "wb");
+	  if( state.depth == 8 )
+	    memcpy( clut, &inBuf->data[len-1024], 1024 );
 	  
-	  if( depth < 16 )
-	    memcpy( clut, &inBuf[len-1024], 1024 );
-	  
-	  fprintf (f, "P6\n# ");
-	  
-//	       fprintf (f, "%s (created on %s)\n", name, fmt_date ( time( NULL )));
-	  fprintf (f, "%s\n", fname );
-	  
-	  fprintf (f, "%d %d\n255\n", w, h );
-	  
-	  switch( depth )
+	  switch( state.depth )
 	    {
-	     case 8:
-	       for( i = 0; i < h*w; i++)
+	     case 1:
+	     case 2:
+	     case 4:
+	       for( i = 0; i < state.h*state.w/(8/state.depth); i++)
 		 {
-		    fwrite( 1 + (char *)&clut[pixelBuf[i]], 1, 1, f);
-		    fwrite( 2 + (char *)&clut[pixelBuf[i]], 1, 1, f);
-		    fwrite( 3 + (char *)&clut[pixelBuf[i]], 1, 1, f);
+		    for( j=(8/state.depth-1), k=0; j >= 0; j--, k++ )
+		      {
+			 /* get right bits */
+			 val = ((pixelBuf->data[i] >> (j * state.depth)) & mask);
+			 /* invert */
+			 val = mask - val;
+			 /* stretch */
+			 val *= (255/mask);
+			 
+			 state.pix_map[3*(i*(8/state.depth)+k)] = val;
+			 state.pix_map[3*(i*(8/state.depth)+k)+1] = val;
+			 state.pix_map[3*(i*(8/state.depth)+k)+2] = val;
+			 
+		      }
+		 }
+	       break;
+	       
+	     case 8:
+	       for( i = 0; i < state.h*state.w; i++)
+		 {
+		    state.pix_map[3*i] = 
+		      *(1 + (char *)&clut[pixelBuf->data[i]]);
+		    state.pix_map[3*i+1] = 
+		      *(2 + (char *)&clut[pixelBuf->data[i]]);
+		    state.pix_map[3*i+2] = 
+		      *(3 + (char *)&clut[pixelBuf->data[i]]);
 		 }
 	       break;
 	       
 	     case 16:
-	       for( i = 0; i < h*w; i++)
+	       for( i = 0; i < state.h*state.w; i++)
 		 {
-		    val = pixelBuf[i*2] & 0xF8;
-		    fwrite( &val, 1, 1, f);
-		    val = (( pixelBuf[i*2] & 0x07 ) << 5 ) 
-		      + (( pixelBuf[i*2+1] & 0xE0 ) >> 3 );
-		    fwrite( &val, 1, 1, f);
-		    val = ( pixelBuf[i*2+1] & 0x1F ) << 3;
-		    fwrite( &val, 1, 1, f);
+		    state.pix_map[i*3] = pixelBuf->data[i*2] & 0xF8;
+		    state.pix_map[i*3+1] = ((pixelBuf->data[i*2] & 0x07 ) << 5)
+		      + (( pixelBuf->data[i*2+1] & 0xE0 ) >> 3 );
+		    state.pix_map[i*3+2] = ( pixelBuf->data[i*2+1] & 0x1F ) << 3;
 		 }
 	       break;
 	       
@@ -343,13 +459,22 @@ void WritePictures (int sd, int db, int type )
 	       break;
 	    }
 	  
-	  free( pixelBuf );
+	  if( type == OUT_PPM )
+	    write_ppm( fname, &state );
+	  else
+	    write_png( fname, &state );	    
+
+	  pi_buffer_free (pixelBuf);
+//	  free( pixelBuf );
+	  free( state.pix_map );
 	  
-	  fclose (f);
+//	  fclose (f);
        }
    else
      return;
-    
+
+   pi_buffer_free (inBuf);
+
 }
 
 int main (int argc, char *argv[])
