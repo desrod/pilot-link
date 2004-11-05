5efdf193c5af6d4236f0b4c6013f6528490427ef
Adriaan de Groot <adridg@cs.kun.nl>
2004-11-05 22:45:45 +0000

    Port to popt / userland. This was a bit of a mess, seems earlier it handled multiple files, which has been mercilessly ripped out (and made buggy in the process).
    TODO: Check if the WriteUserInfo is needed.

--- src/install-memo.c
+++ src/install-memo.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * install-memo.c: Palm MemoPad Record Syncronization Conduit
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -17,7 +17,6 @@
  *
  */
 
-#include "popt.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
@@ -30,35 +29,14 @@
 #include "pi-memo.h"
 #include "pi-header.h"
 
+#include "userland.h"
+
 poptContext po;
 
 #ifndef HAVE_BASENAME
 /* Yes, this isn't efficient... scanning the string twice */
 #define basename(s) (strrchr((s), '/') == NULL ? (s) : strrchr((s), '/') + 1)
 #endif
-
-/***********************************************************************
- *
- * Function:    display_help
- *
- * Summary:     Print out the --help options and arguments  
- *
- * Parameters:  None
- *
- * Returns:     Nothing
- *
- ***********************************************************************/
-static void display_help(char *progname)
-{
-	printf("Installs a new memo in a Palm Computing Device\n\n");
-
-	printf("Usage: %s -p <port> [-rt] [-c category] file [file] ...\n\n", 
-		basename(progname));
-
-	poptPrintHelp(po, stderr, 0);
-
-	exit(EXIT_SUCCESS);
-}
 
 
 int main(int argc, char *argv[])
@@ -70,63 +48,79 @@ int main(int argc, char *argv[])
 		replace		= 0,
 		db,
 		i		= 0,
-		ReadAppBlock, 
+		ReadAppBlock,
 		memo_size,
 		preamble;
-	
-	char 	*port		= NULL,
+
+	char
 		*memo_buf	= NULL,
-		*progname	= argv[0],
 		*category_name	= NULL,
 		*filename	= NULL,
 		buf[0xffff];
-	
+
         struct 	PilotUser User;
         struct 	MemoAppInfo mai;
 	struct  stat sbuf;
 
 	FILE *f;
-	
+
         const struct poptOption options[] = {
-                { "port",     'p', POPT_ARG_STRING, &port, 0, "Use device <port> to communicate with Palm", "<port>"},
-                { "help",     'h', POPT_ARG_NONE,   0, 'h', "Display this information"},
-                { "version",  'v', POPT_ARG_NONE,   0, 'v', "Display version information"},
+		USERLAND_RESERVED_OPTIONS
                 { "category", 'c', POPT_ARG_STRING, &category_name, 0, "Place the memo entry in this category (category must already exist)", "name"},
                 { "replace",  'r', POPT_ARG_NONE,   &replace, 0, "Replace all memos in specified category"},
                 { "title",    't', POPT_ARG_NONE,   &add_title, 0, "Use the filename as the title of the Memo entry"},
-		{ "file",     'f', POPT_ARG_STRING, &filename, 0, "File containing the target memo entry"}, 
-                POPT_AUTOHELP
-                { NULL, 0, 0, NULL, 0 }
+		{ "file",     'f', POPT_ARG_STRING, &filename, 0, "File containing the target memo entry"},
+                POPT_TABLEEND
         };
 
         po = poptGetContext("install-memo", argc, (const char **) argv, options, 0);
+	poptSetOtherOptionHelp(po," [-p port] <memo options> -f filename\n\n"
+		"    Adds a single memo from a file to the memo database.\n\n");
 
-        if (argc < 2) {
-                display_help(progname);
-                exit(1);
-        }
-
-        while ((po_err = poptGetNextOpt(po)) != -1) {
-                switch (po_err) {
-			
-		case 'v':
-			print_splash(progname);
-			return 0;
-		default:
-			display_help(progname);
-			return 0;
-		}
+        while ((po_err = poptGetNextOpt(po)) >= 0) {
 	}
-		
+	if (po_err < -1) userland_badoption(po,po_err);
+
 	if (replace && !category_name) {
-		printf("%s: memo category required when specifying replace\n",
-			progname);
-		display_help(progname);
+		fprintf(stderr,"   ERROR: memo category required when specifying replace\n");
+		return 1;
+	}
+	if (!filename) {
+		fprintf(stderr,"   ERROR: must specify a file with -f filename\n");
+		return 1;
 	}
 
-	stat(filename, &sbuf);
-	if (sbuf.st_size > 65490) {
-		fprintf(stderr, "\n");
+	if (stat(filename, &sbuf) <0) {
+		fprintf(stderr,"   ERROR: cannot determine size of file '%s'\n",filename);
+		return 1;
+	}
+
+	f = fopen(filename, "rb");
+	if (f == NULL) {
+		fprintf(stderr,"   ERROR: Unable to open %s (%s)\n\n",
+			filename, strerror(errno));
+		return 1;
+	}
+
+	memo_size = sbuf.st_size;
+	preamble = add_title ? strlen(filename) + 1 : 0;
+	memo_buf = malloc(memo_size + preamble + 16);
+	if (!memo_buf) {
+		fprintf(stderr,"   ERROR: cannot allocate memory for memo (%s)\n",
+			strerror(errno));
+		return 1;
+	}
+
+	memset(memo_buf,0,memo_size+preamble+16);
+	if (preamble)
+		sprintf(memo_buf, "%s\n", filename);
+	fread(memo_buf + preamble+1, memo_size, 1, f);
+	fclose(f);
+
+	memo_size += preamble + 1;
+
+	if (memo_size > 65490) {
+		fprintf(stderr, "   ERROR:\n");
 		fprintf(stderr, "   File is larger than the allowed size for Palm memo size. Please\n");
 		fprintf(stderr, "   decrease the file size to less than 65,490 bytes and try again.\n\n");
 		fprintf(stderr, "   Files larger than 4,096 bytes and less than 65,490 bytes may be\n");
@@ -134,24 +128,26 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "   due to Palm limitationis.\n\n");
 		return 1;
 	}
-	
-	sd = pilot_connect(port);
+
+
+	sd = userland_connect();
 	if (sd < 0)
 		goto error;
 
 	if (dlp_OpenConduit(sd) < 0)
 		goto error_close;
-	
+
 	dlp_ReadUserInfo(sd, &User);
 	dlp_OpenConduit(sd);
 
 	/* Open the Memo Pad's database, store access handle in db */
 	if (dlp_OpenDB(sd, 0, 0x80 | 0x40, "MemoDB", &db) < 0) {
+		fprintf(stderr, "   ERROR:\n");
 		fprintf(stderr, "   Unable to open MemoDB. Please make sure you have hit the MemoPad\n");
 		fprintf(stderr, "   button at least once, to create a MemoDB.pdb file on your Palm,\n");
 		fprintf(stderr, "   then sync again.\n\n");
 		dlp_AddSyncLogEntry(sd, "Unable to open MemoDB.\n");
-		exit(1);
+		goto error_close;
 	}
 
 	ReadAppBlock = dlp_ReadAppBlock(sd, db, 0, (unsigned char *) buf, 0xffff);
@@ -180,45 +176,22 @@ int main(int argc, char *argv[])
 		category = 0;	/* Unfiled */
 	}
 
-	f = fopen(filename, "rb");
 
-	if (f == 0) {
-		printf("   Unable to open %s (%s), skipping...\n\n",
-			filename, strerror(errno));
-	}
 
-	fseek(f, 0, SEEK_END);
-	memo_size = ftell(f);
-	fseek(f, 0, SEEK_SET);
 
-	if ((sbuf.st_size < 65490) && (sbuf.st_size > 4096)) {
+
+	dlp_WriteRecord(sd, db, 0, 0, category, memo_buf, -1, 0);
+	free(memo_buf);
+
+
+	if ((memo_size < 65490) && (memo_size > 4096)) {
 		fprintf(stderr, "   This file was synconized successfully, but will remain uneditable,\n");
 		fprintf(stderr, "   because it is larger than the Palm limitation of 4,096 bytes in\n");
 		fprintf(stderr, "   size.\n\n");
 
-	} else if ((sbuf.st_size < 4096) && (sbuf.st_size > 0)) {
+	} else if (memo_size < 4096) {
 		fprintf(stderr, "   File '%s' was synchronized to your Palm device\n\n", filename);
 	}
-
-	preamble = add_title ? strlen(filename) + 1 : 0;
-
-	memo_buf = malloc(memo_size + preamble + 1);
-
-	if (memo_buf == NULL) {
-		perror("malloc()");
-		exit(1);
-	}
-
-	if (preamble)
-		sprintf(memo_buf, "%s\n", filename);
-
-	fread(memo_buf + preamble, memo_size, 1, f);
-
-	memo_buf[memo_size + preamble] = '\0';
-
-	dlp_WriteRecord(sd, db, 0, 0, category, memo_buf, -1, 0);
-	free(memo_buf);
-	fclose(f);
 
 	/* Close the database */
 	dlp_CloseDB(sd, db);
@@ -229,16 +202,16 @@ int main(int argc, char *argv[])
 	User.lastSyncDate = User.successfulSyncDate;
 	dlp_WriteUserInfo(sd, &User);
 
-	dlp_AddSyncLogEntry(sd, "Successfully wrote memo(s) to Palm.\n"
+	dlp_AddSyncLogEntry(sd, "Successfully wrote a memo to Palm.\n"
 				"Thank you for using pilot-link.\n");
 
 	dlp_EndOfSync(sd, 0);
 	pi_close(sd);
 	return 0;
- 
+
 error_close:
 	pi_close(sd);
-	
+
 error:
 	return -1;
 }
