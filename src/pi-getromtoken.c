cbdce014694184338347050d7e35ae302b5fa147
Adriaan de Groot <adridg@cs.kun.nl>
2004-11-12 23:13:14 +0000

    Userlandify

--- src/pi-getromtoken.c
+++ src/pi-getromtoken.c
@@ -1,6 +1,6 @@
-/* 
+/*
  * pi-getromtoken.c:  Retrieve a rom token from a device.
- * 
+ *
  * Copyright (C) 2002, Owen Stenseth
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -26,36 +26,9 @@
 #include "pi-header.h"
 #include "pi-source.h"
 #include "pi-syspkt.h"
+#include "pi-util.h"
+#include "userland.h"
 
-
-/***********************************************************************
- *
- * Function:    display_help
- *
- * Summary:     Print out the --help options and arguments
- *
- * Parameters:  None
- *
- * Returns:     Nothing
- *
- ***********************************************************************/
-static void display_help(const char *progname)
-{
-	printf("   Reads a ROM token from a Palm Handheld device\n\n");
-	printf("   Usage: %s -p <port> -t <romtoken>\n\n", progname);
-	printf("   Options:\n");
-	printf("     -p, --port <port>       Use device file <port> to communicate with Palm\n");
-	printf("     -h, --help              Display help information\n");
-	printf("     -v, --version           Display version information\n");
-	printf("     -t <token>              A ROM token to read (i.e. snum)\n\n");
-	printf("   Example: %s -p /dev/pilot -t snum\n\n", progname);
-	printf("   Other tokens you may currently extract are:\n");
-	printf("       adcc:  Entropy for internal A->D convertor calibration\n");
-	printf("       irda:  Present only on memory card w/IrDA support\n");
-	printf("       snum:  Device serial number (from Memory Card Flash ID)\n\n");
-
-	return;
-}
 
 
 int main(int argc, const char *argv[])
@@ -72,77 +45,71 @@ int main(int argc, const char *argv[])
 	size_t	size;
 
 	char    buffer[50];
-	
+
 	poptContext pc;
 
 	struct poptOption options[] = {
-		{"port", 'p', POPT_ARG_STRING, &port, 0,
-		 "Use device file <port> to communicate with Palm", "port"},
-		{"help", 'h', POPT_ARG_NONE, NULL, 'h',
-		 "Display help information", NULL},
-		{"version", 'v', POPT_ARG_NONE, NULL, 'v',
-		 "Show program version information", NULL},
+		USERLAND_RESERVED_OPTIONS
 		{"token", 't', POPT_ARG_STRING, &token, 0,
 		 "A ROM token to read (i.e. snum)", "token"},
 		POPT_TABLEEND
 	};
 
 	pc = poptGetContext("pi-getromtoken", argc, argv, options, 0);
+	poptSetOtherOptionHelp(pc,"\n\n",
+		"   Reads a ROM token from a Palm Handheld device.\n"
+		"   Tokens you may currently extract are:\n"
+		"       adcc:  Entropy for internal A->D convertor calibration\n"
+		"       irda:  Present only on memory card w/IrDA support\n"
+		"       snum:  Device serial number (from Memory Card Flash ID)\n\n"
+		"   Example arguments:\n"
+		"      -p /dev/pilot -t snum\n\n");
 
-	while ((c = poptGetNextOpt(pc)) >= 0) {
-		switch (c) {
-
-		case 'h':
-			display_help(progname);
-			return 0;
-		case 'v':
-			print_splash(progname);
-			return 0;
-		default:
-			display_help(progname);
-			return 0;
-		}
-	}
-
-	if (c < -1) {
-		/* an error occurred during option processing */
-		fprintf(stderr, "%s: %s\n",
-		    poptBadOption(pc, POPT_BADOPTION_NOALIAS),
-		    poptStrerror(c));
+	if (argc < 2) {
+		poptPrintUsage(pc,stderr,0);
 		return 1;
 	}
 
-	
-	if (!token) {
-		display_help(progname);
-		return 0;
+	while ((c = poptGetNextOpt(pc)) >= 0) {
+		fprintf(stderr,"   ERROR: Unhandled option %d.\n",c);
+		return 1;
 	}
 
-	set_long(&long_token, *((long *)token));
-	
-	sd = pilot_connect(port);
+	if (c < -1) {
+		plu_badoption(pc,c);
+	}
+
+
+	if (!token) {
+		fprintf(stderr,"   ERROR: Must specify a token.\n");
+		return 1;
+	}
+
+	long_token = pi_mktag(token);
+
+	sd = plu_connect();
 
 	if (sd < 0)
 		goto error;
 
 	if (dlp_GetROMToken(sd, long_token, buffer, &size) < 0)
 		goto error_close;
-	
+
 	if (dlp_EndOfSync(sd, 0) < 0)
 		goto error_close;
 
 	if (pi_close(sd) < 0)
 		goto error;
 
-	fprintf(stderr, "Token for '%s' was: %s\n", token, buffer);
+	fprintf(stdout, "Token for '%s' was: %s\n", token, buffer);
 
 	return 0;
 
 error_close:
 	pi_close(sd);
-	
+
 error:
 	return -1;
 }
 
-/* vi: set ts=8 sw=4 sts=4 noexpandtab: cin */
+/* vi: set ts=4 sw=4 sts=4 noexpandtab: cin */
