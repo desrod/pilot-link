5b069872cfb17a87862402a147ea15223562c736
Florent Pillet <pilot-link@florentpillet.com>
2004-12-01 15:23:00 +0000

    added shielding against multiple inclusions

--- include/pi-args.h
+++ include/pi-args.h
@@ -1,7 +1,12 @@
+#ifndef __PILOT_ARGS_H__
+#define __PILOT_ARGS_H__
+
 #if ((defined(__STDC__) || defined(SABER)) && !defined(NO_PROTOTYPE)) || defined(__cplusplus) || defined(USE_PROTOTYPE) || defined(CAN_PROTOTYPE)
 #   define PI_ARGS(x)       x
 #   define PI_CONST const
 #else
 #   define PI_ARGS(x)       ()
 #   define PI_CONST
+#endif
+
 #endif
