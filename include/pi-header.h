e29d1598e796562e2b725f6fb05e5b07cf8c76b8
Adriaan de Groot <adridg@cs.kun.nl>
2004-12-20 20:52:50 +0000

    Remove prototypes for functions never created, and mark the remainder deprecated. These prototypes cover functions in connect.c and pi-header.c

--- include/pi-header.h
+++ include/pi-header.h
@@ -1,19 +1,12 @@
 #ifndef _PI_HEADER_H_
 #define _PI_HEADER_H_
 
+#include "pi-util.h"
+
 /* Print the version splash 	*/
-void print_splash(const char *progname);
+void print_splash(const char *progname) PI_DEPRECATED;
 
 /* Connect to the Palm device	*/
-int pilot_connect(const char *port);
-
-/* Open the listen port for connecting to the Palm device */
-int pilot_listen_open(const char *port);
-
-/* Close the listen port */
-void pilot_listen_close(int *parent_sd);
-
-/* Wait for a new connection to the listen port */
-int pilot_connect_wait(int parent_sd, const char *port);
+int pilot_connect(const char *port) PI_DEPRECATED;
 
 #endif /* _PI_HEADER_H_ */
