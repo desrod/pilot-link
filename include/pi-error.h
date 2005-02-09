14195b8ef757ae56e6650d3da9ff9a02dc214bf7
Florent Pillet <pilot-link@florentpillet.com>
2005-02-09 11:51:21 +0000

    proper doxyfication of pi-error.h

--- include/pi-error.h
+++ include/pi-error.h
@@ -2,7 +2,7 @@
  * pi-error.h:  definitions for errors returned by the SOCKET, DLP and
  *              FILE layers
  *
- * Copyright (c) 2004, Florent Pillet.
+ * Copyright (c) 2004-2005, Florent Pillet.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Library General Public License as published by
@@ -23,53 +23,60 @@
 #ifndef _PILOT_ERROR_H_
 #define _PILOT_ERROR_H_
 
-/*
- * Error code definitions
+/** @file pi-error.h
+ *  @brief Error definitions for the errors returned by libpisock's dlp_XXX functions.
  *
- * Note that these error codes are tailored to not conflict
+ * Most dlp_XXX functions return a value that is >= if the function succeeded, or < 0
+ * if there was an error. The error code can be directly tested, and can also be
+ * retrieved using pi_error(). If the error code is #PI_ERR_DLP_PALMOS, you should
+ * read the error code returned by the device using pi_palmos_error().
+ *
+ * @note These error codes are tailored to not conflict
  * with dlpErr* codes defined in dlp.h, and which can be
  * checked using pi_palmos_error()
  *
  */
+enum dlpErrorDefinitions {
+	/* PROTOCOL level errors */
+	PI_ERR_PROT_ABORTED		= -100,		/**< aborted by other end */
+	PI_ERR_PROT_INCOMPATIBLE	= -101,		/**< can't talk with other end */
+	PI_ERR_PROT_BADPACKET		= -102,		/**< bad packet (used with serial protocols) */
 
-/* PROTOCOL level errors */
-#define PI_ERR_PROT_ABORTED			-100		/* aborted by other end */
-#define PI_ERR_PROT_INCOMPATIBLE	-101		/* can't talk with other end */
-#define PI_ERR_PROT_BADPACKET		-102		/* bad packet (used with serial protocols) */
+	/* SOCKET level errors */
+	PI_ERR_SOCK_DISCONNECTED	= -200,		/**< connection has been broken */
+	PI_ERR_SOCK_INVALID		= -201,		/**< invalid protocol stack */
+	PI_ERR_SOCK_TIMEOUT		= -202,		/**< communications timeout (but link not known as broken) */
+	PI_ERR_SOCK_CANCELED		= -203,		/**< last data transfer was canceled */
+	PI_ERR_SOCK_IO			= -204,		/**< generic I/O error */
+	PI_ERR_SOCK_LISTENER		= -205,		/**< socket can't listen/accept */
 
-/* SOCKET level errors */
-#define PI_ERR_SOCK_DISCONNECTED	-200		/* connection has been broken */
-#define PI_ERR_SOCK_INVALID			-201		/* invalid protocol stack */
-#define PI_ERR_SOCK_TIMEOUT			-202		/* communications timeout (but link not known as broken) */
-#define	PI_ERR_SOCK_CANCELED		-203		/* last data transfer was canceled */
-#define PI_ERR_SOCK_IO				-204		/* generic I/O error */
-#define PI_ERR_SOCK_LISTENER		-205		/* socket can't listen/accept */
+	/* DLP level errors */
+	PI_ERR_DLP_BUFSIZE		= -300,		/**< provided buffer is not big enough to store data */
+	PI_ERR_DLP_PALMOS		= -301,		/**< a non-zero error was returned by the device */
+	PI_ERR_DLP_UNSUPPORTED		= -302,		/**< this DLP call is not supported by the connected handheld */
+	PI_ERR_DLP_SOCKET		= -303,		/**< invalid socket */
+	PI_ERR_DLP_DATASIZE		= -304,		/**< requested transfer with data block too large (>64k) */
+	PI_ERR_DLP_COMMAND		= -305,		/**< command error (the device returned an invalid response) */
 
-/* DLP level error */
-#define PI_ERR_DLP_BUFSIZE			-300		/* provided buffer is not big enough to store data */
-#define PI_ERR_DLP_PALMOS			-301		/* a non-zero error was returned by the device */
-#define PI_ERR_DLP_UNSUPPORTED		-302		/* this DLP call is not supported by the connected handheld */
-#define PI_ERR_DLP_SOCKET			-303		/* invalid socket */
-#define	PI_ERR_DLP_DATASIZE			-304		/* requested transfer with data block too large (>64k) */
-#define PI_ERR_DLP_COMMAND			-305		/* command error (the device returned an invalid response) */
+	/* FILE level error */
+	PI_ERR_FILE_INVALID		= -400,		/**< invalid prc/pdb/pqa/pi_file file */
+	PI_ERR_FILE_ERROR		= -401,		/**< generic error when reading/writing file */
+	PI_ERR_FILE_ABORTED		= -402,		/**< file transfer was aborted by progress callback, see pi_file_retrieve(), pi_file_install(), pi_file_merge() */
+	PI_ERR_FILE_NOT_FOUND		= -403,		/**< record or resource not found */
 
-/* FILE level error */
-#define PI_ERR_FILE_INVALID			-400		/* invalid prc/pdb/pqa/pi_file file */
-#define PI_ERR_FILE_ERROR			-401		/* generic error when reading/writing file */
-#define	PI_ERR_FILE_ABORTED			-402		/* file transfer was aborted by progress callback
-												   see pi_file_retrieve(), pi_file_install(), pi_file_merge() */
-#define PI_ERR_FILE_NOT_FOUND		-403		/* record or resource not found */
+	/* GENERIC errors */
+	PI_ERR_GENERIC_MEMORY		= -500,		/**< not enough memory */
+	PI_ERR_GENERIC_ARGUMENT		= -501,		/**< invalid argument(s) */
+	PI_ERR_GENERIC_SYSTEM		= -502		/**< generic system error */
+};
 
-/* GENERIC errors */
-#define PI_ERR_GENERIC_MEMORY		-500		/* not enough memory */
-#define PI_ERR_GENERIC_ARGUMENT		-501		/* invalid argument(s) */
-#define PI_ERR_GENERIC_SYSTEM		-502		/* generic system error */
-
-/* Macros */
-#define IS_PROT_ERR(error)			((error)<=-100 && (error)>-200)
-#define IS_SOCK_ERR(error)			((error)<=-200 && (error)>-300)
-#define IS_DLP_ERR(error)			((error)<=-300 && (error)>-400)
-#define IS_FILE_ERR(error)			((error)<=-400 && (error)>-500)
-#define IS_GENERIC_ERR(error)		((error)<=-500 && (error)>-600)
+/** @name libpisock error management macros */
+/*@{*/
+	#define IS_PROT_ERR(error)	((error)<=-100 && (error)>-200)	/**< Check whether the error code is at protocol level */
+	#define IS_SOCK_ERR(error)	((error)<=-200 && (error)>-300) /**< Check whether the error code is at socket level */
+	#define IS_DLP_ERR(error)	((error)<=-300 && (error)>-400) /**< Check whether the error code is at DLP level */
+	#define IS_FILE_ERR(error)	((error)<=-400 && (error)>-500) /**< Check whether the error code os a file error */
+	#define IS_GENERIC_ERR(error)	((error)<=-500 && (error)>-600) /**< Check whether the error code is a generic error */
+/*@}*/
 
 #endif
