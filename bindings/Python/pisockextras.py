e224b82170714589a4443a5628721a766ecc684e
Florent Pillet <pilot-link@florentpillet.com>
2005-02-21 13:20:27 +0000

    fixed loop as dlp_ReadDBList_ can return None

--- bindings/Python/pisockextras.py
+++ bindings/Python/pisockextras.py
@@ -7,12 +7,13 @@ def dlp_ReadDBList(sd, cardno=0, flags=None):
         flags = pisock.dlpDBListRAM
     while True:
         try:
-            for db in pisock.dlp_ReadDBList_(sd, cardno, pisock.dlpDBListMultiple | flags, i):
+            lst = pisock.dlp_ReadDBList_(sd, cardno, pisock.dlpDBListMultiple | flags, i)
+            if lst==None or len(lst)==0:
+                return ret
+            for db in lst:
                 i = db['index'] + 1
                 ret.append(db)
         except pisock.dlperror:
             if pisock.pi_palmos_error(sd) == pisock.dlpErrNotFound:
                 return ret
-            else:
-                raise
-
+            raise
