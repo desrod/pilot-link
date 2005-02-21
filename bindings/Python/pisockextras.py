1a0f2da4f514d12e8c254324248d5ba1f2d1fbcc
Florent Pillet <pilot-link@florentpillet.com>
2005-02-21 13:39:46 +0000

    tweaks

--- bindings/Python/pisockextras.py
+++ bindings/Python/pisockextras.py
@@ -8,7 +8,7 @@ def dlp_ReadDBList(sd, cardno=0, flags=None):
     while True:
         try:
             lst = pisock.dlp_ReadDBList_(sd, cardno, pisock.dlpDBListMultiple | flags, i)
-            if lst==None or len(lst)==0:
+            if (lst is None) or (len(lst) == 0):
                 return ret
             for db in lst:
                 i = db['index'] + 1
