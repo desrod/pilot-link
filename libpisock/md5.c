44c727f429334bedec605d86412e1cc5db0c66d0
Adriaan de Groot <adridg@cs.kun.nl>
2005-10-17 20:14:26 +0000

    Fix the address of the FSF in lots (but surely not all) places.

--- libpisock/md5.c
+++ libpisock/md5.c
@@ -1,4 +1,6 @@
 /*
+ * LICENSE: Public Domain
+ *
  * This code implements the MD5 message-digest algorithm.  The algorithm is
  * due to Ron Rivest.  This code was written by Colin Plumb in 1993, no
  * copyright is claimed.  This code is in the public domain; do with it what
